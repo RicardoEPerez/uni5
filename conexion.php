c2a58f200737cf8d841be9206ee0b6fa7406c11e
RicardoEPerez <79209305+RicardoEPerez@users.noreply.github.com>
2021-05-27 19:32:03 -0500

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
     $server = "den1.mysql6.gear.host";
     $username = "uni5";
     $password = "Vh3R58185--H";
-    $database = "sistema";
+    $database = "uni5";
     //Conexion
     $db = mysqli_connect($server,$username,$password,$database);
 ?>
